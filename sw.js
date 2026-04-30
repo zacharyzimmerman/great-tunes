b3685a069efd0425ce266ab012ac2ff5a451f2b2
zacharyzimmerman <zacharyzimmerman@users.noreply.github.com>
2026-04-30 03:57:25 +0000

    deploy: 8634fd587c6e6800e272d150961e853f340ae28f

--- sw.js
+++ sw.js
@@ -1,35 +1,9 @@
-const CACHE_NAME = 'tunes-v0.1.0';
-const PRE_CACHE = [
-  "./",
-  "./styles.css",
-  "./app.js",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(PRE_CACHE)));
-  self.skipWaiting();
-});
-
+// Self-destructing service worker: unregisters itself and clears all caches.
+// Deployed to clean up old cached versions on existing installs.
+self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
-  );
-  clients.claim();
-});
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    fetch(e.request)
-      .then((resp) => {
-        const clone = resp.clone();
-        caches.open(CACHE_NAME).then((c) => c.put(e.request, clone));
-        return resp;
-      })
-      .catch(() => caches.match(e.request))
+    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
+      .then(() => self.registration.unregister())
   );
 });
